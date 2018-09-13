adb6ba4b919d45abc84cd3716e3fdeed6ac7c4dc
paul.chen <paul.chen@scmp.com>
2018-09-13 15:27:49 +0800

    bug fixes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/drawings', to: 'drawings#index'
+  root to: "drawings#index"
   
   resources :drawings
 end
