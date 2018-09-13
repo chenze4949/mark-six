507cb81f890bfaf123b6f06653b5c27f0f733e71
Paulgor <chanzakbi@gmail.com>
2018-09-14 00:39:34 +0800

    finish ticket creation

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -3,18 +3,17 @@ class TicketsController < ApplicationController
 
   def create
     @drawing = Drawing.find(params[:drawing_id])
-    ticket = Ticket.create(drawing_id: @drawing.id, user_id: current_user.id)
-    Pick.create(ticket_id: ticket.id, ball_number: ticket_params['pick_1'])
-    Pick.create(ticket_id: ticket.id, ball_number: ticket_params['pick_2'])
-    Pick.create(ticket_id: ticket.id, ball_number: ticket_params['pick_3'])
-    Pick.create(ticket_id: ticket.id, ball_number: ticket_params['pick_4'])
-    Pick.create(ticket_id: ticket.id, ball_number: ticket_params['pick_5'])
-    Pick.create(ticket_id: ticket.id, ball_number: ticket_params['pick_6'])
+    if params[:ball_numbers].kind_of?(Array)
+      ticket = Ticket.create(drawing_id: @drawing.id, user_id: current_user.id)
+      for ball_number in params[:ball_numbers]
+        Pick.create(ticket_id: ticket.id, ball_number: ball_number)
+      end
+    end
     redirect_to drawing_path(@drawing)
   end
 
   private
     def ticket_params
-      params.require(:ticket).permit(:pick_1, :pick_2, :pick_3, :pick_4, :pick_5, :pick_6)
+      params.permit(:ball_numbers)
     end
 end
